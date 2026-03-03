623c18a261a89f5807c11890706e3cb3fcbe2405
Sean Wotherspoon <seanjeemon@gmail.com>
2026-03-03 13:24:36 -0800

    Implement image preprocessor

--- src/preprocess/face_preprocess.py
+++ src/preprocess/face_preprocess.py
@@ -3,8 +3,10 @@ import numpy as np
 import os
 from src.config import IMAGE_SIZE
 
+
 CASCADE_PATH = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
+
 
 def detect_largest_face(grayscale_img):
     """
@@ -30,14 +32,44 @@ def detect_largest_face(grayscale_img):
     # return largest face
     return max(faces, key=lambda rect: rect[2] * rect[3])
 
-def preprocess_image(image_path):
+
+def preprocess_image(img_path):
     """
     Load image, detect face, crop, resize, normalize, and flatten.
 
     Parameters:
-        image_path (str): The file path of the image to preprocess.
+        img_path (str): The file path of the image to preprocess.
 
     Returns:
         np.ndarray: A 1D flattened array of shape (IMAGE_SIZE * IMAGE_SIZE,).
     """
-    
+    if not os.path.exists(img_path):
+        raise FileNotFoundError(f"Image not found at path: {img_path}.")
+
+    # 1. load image
+    img = cv2.imread(img_path)
+
+    if img is None:
+        raise ValueError("Could not load image.")
+
+    # 2. convert to grayscale
+    grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+    # 3. detect face
+    face_box_tuple = detect_largest_face(grayscale_img)
+
+    if face_box_tuple is not None:
+        # use bounding box dimensions from largest face detection
+        x, y, w, h = face_box_tuple
+        cropped_img = grayscale_img[y:y+h, x:x+w]
+    else:
+        # otherwise use full image
+        cropped_img = grayscale_img
+
+    # 4. resize
+    resized_img = cv2.resize(cropped_img, (IMAGE_SIZE, IMAGE_SIZE))
+
+    # 5. normalize and flatten
+    normalized_img = resized_img.astype(np.float32) / 255.0
+    return normalized_img.flatten()
