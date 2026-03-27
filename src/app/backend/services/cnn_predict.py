317d8d37f7ee1d0c020642560fa4440ee4f0d6af
A Lam <makomako11711@gmail.com>
2026-03-27 10:26:33 -0700

    Add documentation and update variable names

--- src/app/backend/services/cnn_predict.py
+++ src/app/backend/services/cnn_predict.py
@@ -7,7 +7,16 @@ from src.config import LABELS, IMAGE_SIZE
 
 
 def cnn_predict(model, image_file) -> dict:
-    # Handle image file for CNN
+    """
+    Predict emotion and confidence of an image using a CNN model.
+
+    Parameters:
+        model: The loaded CNN model.
+        image_file: The image file.
+    Returns:
+        dict: Prediction results. {enotion label, confidence, source}
+    """
+    # Handle image file (FileStorage -> np.array -> tensor)
     image_bytes = image_file.read()
     image_array = np.frombuffer(image_bytes, np.uint8)
     image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
@@ -18,14 +27,14 @@ def cnn_predict(model, image_file) -> dict:
     # Compute CNN prediction results
     logits = model(processed_image_tensor)
     probability = nn.Softmax(dim=1)(logits)
-    confidence, emotion = probability.topk(1, dim=1)
+    confidence, pred_emotion = probability.topk(1, dim=1)
 
     # print("--- Prediction Results ---")    
     # for i, label in enumerate(LABELS):
     #     print(f" - {label}: {probability[0][i]:.4f}")
     
     return {
-        "emotion": LABELS[emotion],
+        "emotion": LABELS[pred_emotion],
         "confidence": confidence.item(),
         "source": "CNN",
     }
--- src/app/backend/services/load_cnn.py
+++ src/app/backend/services/load_cnn.py
@@ -4,6 +4,14 @@ from src.training.train_cnn import EmotionClassifierCNN
 from src.config import LABELS
 
 def load_cnn():
+    """
+    Load an existing CNN model.
+
+    Parameters:
+        None
+    Returns:
+        EmotionClassifierCNN | None: The loaded model for evaluation or None if model is not found.
+    """
     model_path = "models/cnn_model.pt"
     
     model_exists = os.path.exists(model_path)
@@ -13,7 +21,7 @@ def load_cnn():
 
     device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
     model = EmotionClassifierCNN(num_classes=len(LABELS)).to(device)
-    model.load_state_dict(torch.load(model_path, map_location=device, weights_only = False))
+    model.load_state_dict(torch.load(model_path, map_location=device, weights_only=False))
     model.eval()
 
     return model
